4fbe7c701b9b17e945876a8002f8fa6f23395bc8
baiching <bachmarma@gmail.com>
2025-02-18 11:58:35 +0100

    Adding Dependencies

---
+++ signUpAndSignIn/src/main/java/baiching/signUpAndSignIn/SignUpAndSignInApplication.java
@@ -0,0 +1,13 @@
+package baiching.signUpAndSignIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpAndSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignUpAndSignInApplication.class, args);
+	}
+
+}
