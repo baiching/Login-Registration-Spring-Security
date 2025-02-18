8ba0dc0cbc63c01d36b6dc947b929f4dc888d997
baiching <bachmarma@gmail.com>
2025-02-18 12:42:27 +0100

    Added getters and setters in Role entity

--- signUpAndSignIn/src/main/java/baiching/signUpAndSignIn/models/Role.java
+++ signUpAndSignIn/src/main/java/baiching/signUpAndSignIn/models/Role.java
@@ -29,4 +29,12 @@ public class Role {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 }
